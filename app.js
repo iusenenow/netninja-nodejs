5ec6d642ad0e4c701448fac233e3bf202398c7c5
Wei Zhang <iusenenow@gmail.com>
2020-09-02 21:52:31 +1000

    update

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+// register view engine
+app.set('view engine', 'ejs')
+
+// middleware & static files
+app.use(morgan('dev'))
+app.use(express.static('public'))
+
 // requests
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname })
+  // res.sendFile('./views/index.html', { root: __dirname })
+  const blogs = [
+    { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
+  ]
+  res.render('index', { title: 'Home', blogs })
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname })
+  // res.sendFile('./views/about.html', { root: __dirname })
+  res.render('about', { title: 'About' })
+})
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new Blog' })
 })
 
 // redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about')
-})
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about')
+// })
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname })
+  res.status(404).render('404', { title: '404' })
+  // sendFile('./views/404.html', { root: __dirname })
 })
 
 // listen for requests

ea4743674f81e61700c2ad97fc0843e3d513d763
Wei Zhang <iusenenow@gmail.com>
2020-09-03 12:44:03 +1000

    finish touch

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
-const app = express()
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // middleware & static files
-app.use(morgan('dev'))
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-// requests
+// routes
 app.get('/', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname })
-  const blogs = [
-    { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-    { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' }
-  ]
-  res.render('index', { title: 'Home', blogs })
-})
+  res.redirect('/blogs');
+});
 
 app.get('/about', (req, res) => {
-  // res.sendFile('./views/about.html', { root: __dirname })
-  res.render('about', { title: 'About' })
-})
+  res.render('about', { title: 'About' });
+});
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new Blog' })
-})
-
-// redirects
-// app.get('/about-us', (req, res) => {
-//   res.redirect('/about')
-// })
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' })
-  // sendFile('./views/404.html', { root: __dirname })
-})
-
-// listen for requests
-app.listen(3000)
+  res.status(404).render('404', { title: '404' });
+});
